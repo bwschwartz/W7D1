0d8ade3dd358b47fad408a2d835728cfa3f03890
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-29 15:36:06 -0400

    Start UsersController

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice/app/models/user.rb
@@ -0,0 +1,61 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  sesion_token    :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+  validates :password_digest, presence: true
+  validates :session_token, :username, presence: true, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  def generate_unique_session_token
+    token = SecureRandom::urlsafe_base64
+    while User.find_by(session_token: token)
+      token = SecureRandom::urlsafe_base64
+    end
+    token
+  end
+
+
+
+end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice/config/routes.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice/config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   resources :cats, except: :destroy do
     resources :cat_rental_requests, only: [:new]
   end
+
+  resources :sessions, only: %i(new create destroy)
+  resources :users, only: %i(index create new destroy show)
+
+
   resources :cat_rental_requests, only: [:new, :create] do
     member do
       post :approve
